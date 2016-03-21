c877f36efb8e244248f2af54862bdb7b71e54904
Artem Tarasov <artem.tarasov@embl.de>
2016-03-21 11:48:40 +0100

    fix OS X build

--- make_osx_wheel.sh
+++ make_osx_wheel.sh
@@ -8,7 +8,7 @@ cd ims-cpp && mkdir -p build && cd build &&\
     cmake -DCMAKE_C_COMPILER=gcc-5 -DCMAKE_CXX_COMPILER=g++-5 -DCMAKE_BUILD_TYPE=Release .. &&\
     make ims_cffi && cd ../../ &&\
     /usr/local/bin/python setup.py bdist_wheel &&\
-    delocate-wheel dist/cpyMSpec*.whl
+    delocate-wheel dist/cpyImagingMSpec*.whl
 
 # the resulting wheel must be uploaded to PyPI using 'twine upload' command
 # (this is also recommended due to security concerns over 'python setup.py upload',
